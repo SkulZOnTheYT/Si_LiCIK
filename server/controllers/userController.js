fbabb793a42f435dcf3b50a386fff96416fe86f4
OkelahBegitu <muhammadfardan706@gmail.com>
2025-03-11 20:47:32 +0800

    Menambahkan fitur Update User, Debuging, perubahan URL MONGO_DB

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,16 +2,28 @@ const User = require("../models/user");
 
 // Get All Users
 exports.getUsers = async (req, res) => {
+    try{
     const users = await User.find();
     res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: "Gagal Menggambil semua users" });
+    }
 };
 
 // Create User
 exports.createUser = async (req, res) => {
+    try{
     const { name, email } = req.body;
     const newUser = new User({ name, email });
+    const Alreadyuser = await User.findOne({ name, email});
+    if (Alreadyuser){
+        return res.status(400).json({ message: "Nama atau Email sudah di gunakan"});
+    }
     await newUser.save();
     res.json(newUser);
+    } catch {
+        res.status(500).json({ error: "Gagal Membuat User" });
+    }
 };
 
 // Delete User
@@ -19,4 +31,21 @@ exports.deleteUser = async (req, res) => {
     const { id } = req.params;
     await User.findByIdAndDelete(id);
     res.json({ message: "User deleted" });
+};
+
+//Update User
+exports.Updateuser = async (req, res) => {
+    try{
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const Alreadyuser = await User.findOne({ name, email});
+    if (Alreadyuser){
+        return res.status(400).json({ message: "Nama Atau Email yang sudah di gunakan tidak dapat di ubah"});
+    }
+    const updatedUser = await User.findByIdAndUpdate(id, { name, email }, { new: true });
+    res.json(updatedUser);
+    }
+    catch (err){
+        res.status(500).json({error: "gagal menggupdate"})
+    }
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getUsers, createUser, deleteUser } = require("../controllers/userController");
+const { getUsers, createUser, deleteUser, Updateuser } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.post("/users", createUser);
 router.delete("/users/:id", deleteUser);
+router.put("/:id", Updateuser);
 
 module.exports = router;    
